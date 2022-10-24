eb95b40e947c8f97545b6272abbf4ec78de3e3e4
TheComputerizer <dcpixelation42@gmail.com>
2022-10-23 19:01:24 -0500

    fix dependency

--- src/main/java/mods/thecomputerizer/darkstone/Darkstone.java
+++ src/main/java/mods/thecomputerizer/darkstone/Darkstone.java
@@ -11,7 +11,7 @@ public class Darkstone {
     public static final String MODID = "darkstone";
     public static final String NAME = "Darkstone";
     public static final String VERSION = "1.12.2-1.0";
-    public static final String DEPENDENCIES = "mixinbooter";
+    public static final String DEPENDENCIES = "required-after:mixinbooter";
     public static Logger LOGGER = LogManager.getLogger(MODID);
 
     @EventHandler
