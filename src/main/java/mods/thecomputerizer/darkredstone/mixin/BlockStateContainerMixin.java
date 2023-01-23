ea7cedca6126648e368ca9e1f58974bf73902827
TheComputerizer <dcpixelation42@gmail.com>
2023-01-23 17:22:42 -0600

    refmap generates and redstone torches are now dark :)

--- src/main/java/mods/thecomputerizer/darkredstone/mixin/BlockStateContainerMixin.java
+++ src/main/java/mods/thecomputerizer/darkredstone/mixin/BlockStateContainerMixin.java
@@ -1,10 +1,7 @@
 package mods.thecomputerizer.darkredstone.mixin;
 
-import mods.thecomputerizer.darkredstone.Darkstone;
 import net.minecraft.block.*;
 import net.minecraft.block.state.BlockStateContainer;
-import net.minecraft.util.math.BlockPos;
-import net.minecraft.world.IBlockAccess;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -12,30 +9,20 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-@Mixin(value = BlockStateContainer.StateImplementation.class)
+@Mixin(value = BlockStateContainer.StateImplementation.class, priority = 1002)
 public class BlockStateContainerMixin {
 
     @Shadow
     @Final
     private Block block;
 
-    @Inject(at = @At(value = "HEAD"), method = "getLightValue()I", cancellable = true)
+    @Inject(method = "getLightValue", at = @At(value = "HEAD"), cancellable = true)
     private void darkstone_getLightValue(CallbackInfoReturnable<Integer> cir) {
         if(block instanceof BlockRedstoneTorch || block instanceof BlockRedstoneWire
                 || block instanceof BlockRedstoneRepeater || block instanceof BlockRedstoneComparator
                 || block instanceof BlockCompressedPowered) {
-            Darkstone.LOGGER.info("Successfully altered a light value");
             cir.setReturnValue(0);
         }
     }
-
-    @Inject(at = @At(value = "HEAD"), method = "getLightValue(Lnet/minecraft/world/IBlockAccess;Lnet/minecraft/util/math/BlockPos;)I", cancellable = true)
-    private void darkstone_getLightValueWorld(IBlockAccess world, BlockPos pos, CallbackInfoReturnable<Integer> cir) {
-        if(block instanceof BlockRedstoneTorch || block instanceof BlockRedstoneWire
-                || block instanceof BlockRedstoneRepeater || block instanceof BlockRedstoneComparator
-                || block instanceof BlockCompressedPowered) {
-            Darkstone.LOGGER.info("Successfully altered a light value");
-            cir.setReturnValue(0);
-        }
-    }
+    //, target = "Lnet/minecraft/block/state/BlockStateContainer/StateImplementation;func_185906_d()I"
 }
